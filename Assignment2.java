c6ac1883eadcc3cef581a5e76988d3b85ca1c90e
ilqer <ilker.yigitel@ug.bilkent.edu.tr>
2023-09-20 15:45:33 +0300

    Completed Part C

--- Assignment2.java
+++ Assignment2.java
@@ -10,10 +10,33 @@ public class Assignment2 {
         }
         return arr1;
     }
-
+    public static int minOfArr(int[] arr){
+        int min;
+        min = arr[0];
+        for(int i = 0; i<arr.length ;  i++){
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+    public static int maxOfArr(int[] arr){
+        int max;
+        max = arr[0];
+        for(int i = 0; i<arr.length ;  i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
     public static void main(String[] args) {
         int[] test = createArray(6);
         System.out.println(Arrays.toString(test));
+        System.out.println(maxOfArr(test));
+        System.out.println(minOfArr(test));
         
     }
 }
+    
+
